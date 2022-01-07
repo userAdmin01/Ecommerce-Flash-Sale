9fe861cb0f1fa3404d52860a18cb3209da40fe1f
DeepuGagan <gagandeepu717@gmail.com>
2022-01-08 02:44:19 +0530

    Ui correction required and image loding error --Pruthvi

---
+++ Checkout-5/checkout.js
@@ -0,0 +1,8 @@
+function onlinecheck(that){
+    if(that.value=="onlinePay"){
+        document.getElementById("ifyes").style.display="block";
+    }
+    else{
+        document.getElementById("ifyes").style.display="none";
+    }
+}
